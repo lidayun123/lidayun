bb075504c8fca41a4f4117e0e4421c25d2283ebc
lidayun2003 <2216662890@qq.com>
2020-09-04 19:01:51 +0800

    other helloword

---
+++ src/cn/ldy/otherHelloWord.java
@@ -0,0 +1,13 @@
+package cn.ldy;
+
+public class otherHelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("other HelloWord");
+	}
+
+}
